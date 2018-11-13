8a39a7cf9258e54f549076dc0b4bdf5b5b39124a
Akop K <akop87@gmail.com>
2018-11-12 22:00:56 -0700

    some JS variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+var players = {
+    p1 : {
+        id:     "",
+        name:   "",
+        wins:   0,
+        losses: 0
+    },
+    p2 : {
+        id:     "",
+        name:   "",
+        wins:   0,
+        losses: 0
+    },
+    activeSpectators: [],
+    totalConnections: 0
+};
+var user = {
+    role: "",
+    key:  ""
+};
