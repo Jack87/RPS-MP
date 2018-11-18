bded2080b22b27db6c980107c55f0490d81f58a4
Akop K <akop87@gmail.com>
2018-11-17 22:57:51 -0700

    cycling through choices but has a pause

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,3 +18,47 @@ var user = {
     role: "",
     key:  ""
 };
+
+if (1==1) {
+    jQuery(function($){
+    
+    // Cycle plugin
+    $('.slides').cycle({
+        fx:     'none',
+        speed:   250,
+        timeout: 5,
+        requeueTimeout: 1         
+
+    });
+    
+    // Pause &amp; play on hover
+    $('.slideshow-block').hover(function(){
+        $(this).find('.slides').addClass('active').cycle("resume");
+        $(this).addClass('bg-none');
+    }, function(){
+        $(this).find('.slides').removeClass('active').cycle("pause");
+        $(this).removeClass('bg-none');
+
+    });
+    
+    });
+    };
+// if (1==1) { 
+//     jQuery(function($){
+
+//         // Cycle plugin
+//         $('.slides').cycle({
+//             fx:     'none',
+//             speed:   150,
+//             timeout: 5
+//         }).cycle("pause");
+
+//         // Pause &amp; play on hover
+//         $('.slideshow-block').hover(function(){
+//             $(this).find('.slides').addClass('active').cycle('resume');
+//         }, function(){
+//             $(this).find('.slides').removeClass('active').cycle('pause');
+//         });
+
+//     });
+// };
